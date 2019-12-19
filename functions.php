08c78d4425fe30355698122dd0f2c785808de339
jarnalyrkar <joachimsolstrom@gmail.com>
2019-12-19 11:37:02 +0100

    Fix functions.php

--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@
 $base_dir = get_stylesheet_directory();
 // Enqueue dist/css and dist/js
 //   do not change this without also changing gulpfile.js and webpack.config.js to match
-require_once($base_dir . 'inc/functions/enqueue_scripts.php');
+// require_once($base_dir . 'inc/functions/enqueue_scripts.php');
 // Remove emojis
-require_once($base_dir . 'inc/functions/disable_emojis.php');
+require_once($base_dir . '/inc/functions/disable_emojis.php');
 // Remove jquery by default
-require_once($base_dir . 'inc/functions/deregister_jquery.php');
+require_once($base_dir . '/inc/functions/deregister_jquery.php');
 // Add markup to allow for responsive oembeds
-require_once($base_dir . 'inc/functions/add_responsive_embed.php');
+require_once($base_dir . '/inc/functions/add_responsive_embed.php');
 // Add BEM-style classes to menus
-require_once($base_dir . 'inc/functions/add_bem_menu_classes.php');
+require_once($base_dir . '/inc/functions/add_bem_menu_classes.php');
 
 /* Add default main menu */
 function create_main_menu() {
