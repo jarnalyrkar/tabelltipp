2ab46e816d5d9419e4e6cf05cfd604cea0635bd5
jarnalyrkar <joachimsolstrom@gmail.com>
2019-12-20 09:07:44 +0100

    add timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,2 @@
-import MobileMenu from './modules/MobileMenu';
 import sortableList from './modules/sortableList'
-
-//import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
-//import bootstrapSlim from '../../node_modules/jquery/dist/jquery.slim.min';
-
-
-
-/* Instantiate a new object */
-//const mobileMenu = new MobileMenu();
+import timer from './modules/timer';
--- header.php
+++ header.php
@@ -7,7 +7,17 @@ $context['nav'] = wp_nav_menu(array(
   'menu_class' => 'main-nav'
   )
 );
-
+$context['post'] = new Timber\Post();
 $context['logout_link'] = site_url() . '/wp-login.php?action=logout';
 $context['logo'] = get_stylesheet_directory_uri() . '/assets/img/logo.jpg';
+
+$now = new DateTime();
+$now->setTimezone(new DateTimeZone('Europe/Oslo'));
+$future = new DateTime($context['post']->sesongstart, new DateTimeZone('Europe/Oslo'));
+$interval = $future->diff($now);
+
+$context['time_left'] = $interval->format('<span id="days">%a</span> dager, <span id="hours">%h</span> timer, <span id="minutes">%i</span> minutter og <span id="seconds">%s</span> sekunder');
+$context['future'] = $future->format('D M d Y H:i:s O');
+
+
 Timber::render('header.twig', $context);
